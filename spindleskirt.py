f8a621af5a6b0d37d6b043ae379f18e9042412c7
Torsten Martinsen <torsten@bullestock.net>
2025-05-03 18:30:39 +0200

    Add screw holes.

--- spindleskirt.py
+++ spindleskirt.py
@@ -5,15 +5,15 @@ import math
 outer_dia = 78
 inner_dia = 65
 slot_radius = (outer_dia + inner_dia)/4
-slot_width = 1
+slot_width = 1.5
 slot_degrees = 300
-slot_height = 4
-thickness = 6
+thickness = 10
+slot_height = thickness - 2
 
 bottom = (Align.CENTER, Align.CENTER, Align.MIN)
 
 slot = Cylinder(slot_radius + slot_width/2, slot_height)             
-slot -= Cylinder(slot_radius - slot_width/2, slot_height, mode=Mode.SUBTRACT)
+slot -= Cylinder(slot_radius - slot_width/2, slot_height)
 plane = Plane(slot.faces().sort_by(Axis.Z).first).offset(-1)
 slot = plane*slot
 
@@ -23,17 +23,23 @@ p -= Cylinder(inner_dia/2, thickness, mode=Mode.SUBTRACT)
     
 p -= slot
 
-stud_l = 10
-stud = Pos(0, slot_radius + stud_l/2, 0) * Box(10, stud_l, thickness)
+stud_l = 14
+stud = Pos(0, slot_radius + stud_l/2 - 2, 0) * Box(10, stud_l, thickness)
 stud = fillet(stud.edges().filter_by(Axis.Z), radius=1)
 stud = fillet(stud.edges().filter_by(Axis.X), radius=0.5)
 p += stud
 
-slit = Pos(0, slot_radius, 0) * Box(3, 20, thickness)
+slit = Pos(0, slot_radius, 0) * Box(3, 30, thickness)
 p -= slit
 
-hole = Rot(0, 90, 0) * Pos(0, slot_radius + stud_l - thickness/2, 0) * Cylinder(radius=3.2/2, height=20)
+hole = Rot(0, 90, 0) * Pos(0, slot_radius + stud_l - thickness*0.6, 0) * Cylinder(radius=3.2/2, height=20)
 p -= hole
+
+STEP = 30
+for angle in range(STEP//2, 360 + STEP//2, STEP):
+    p -= Pos(slot_radius*math.cos(math.radians(angle)),
+             slot_radius*math.sin(math.radians(angle)),
+             -thickness/2 + 4) * Rot(90, 90 + angle) * Cylinder(radius=1, height=10)
 
 show(p)
 
